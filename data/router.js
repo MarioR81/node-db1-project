df60e4c7983ef6ca882101f8d286539bc496ed76
apple <apple@apples-MacBook-Pro-2.local>
2020-03-16 19:25:59 -0700

    set up post delete, tested, working

--- data/router.js
+++ data/router.js
@@ -18,6 +18,68 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+    db('accounts')
+    .insert(req.body, 'id')
+    .then(ids => {
+        res.status(201).json({ results: ids })
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'sorry, ran into an error' })
+    });
+})
+
+router.get('/:id', (req, res) => {
+    db('accounts')
+    .where({ id: req.params.id })
+    .first()
+    .then(account => {
+        if (account) {
+            res.status(200).json({ data: account })
+        } else {
+            res.status(404).json({ message: 'Account not found!' })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'sorry, ran into an error' })
+    });
+})
+
+router.delete('/:id', (req, res) => {
+    db('accounts')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({ message: 'Record Deleted successfully' })
+        } else {
+            res.status(404).json({ message: 'Account not found!' })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'sorry, ran into an error' })
+    });
+});
+
+
+router.put(':id', (req, res) => {
+    const changes = req.body;
+
+    db('accounts')
+    .where({ id: req.params.id })
+    .update(changes)
+    .then(count => {
+        if (count > 0) {
+            res.status(200).json({ message: 'Account UPDATED successfully' })
+        } else {
+            res.status(404).json({ message: 'Account not found!' })
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'sorry, ran into an error' })
+    });
+})
+
 
 
 module.exports = router;
